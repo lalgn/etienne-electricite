0ddfcb28606bd678b16bbe81d729e5c16360c89e
etienne guerin <etien.guerin@gmail.com>
2025-10-22 15:03:27 +0200

    travail sur photo et sur l'alignement et détail

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -28,7 +28,16 @@ export default function About() {
         {images.map((img, index) => (
           <Col md={4} key={index}>
             <Card className="shadow-sm border-0">
-              <Card.Img variant="top" src={img.src} alt={img.caption} />
+              <Card.Img variant="top"
+              src={img.src}
+              alt={img.caption}
+              style={{
+                    objectFit: "contain", // ne rogne pas l’image
+                    borderRadius: 0, // pas de coins arrondis
+                    width: "100%",
+                    height: "auto", // garde les proportions d’origine
+                  }}
+               />
               <Card.Body>
                 <Card.Text className="text-center text-light">
                   {img.caption}
@@ -45,10 +54,10 @@ export default function About() {
   return (
     <Container className="py-5">
       <h2 className="text-center mb-4"> <i class="bi bi-info-circle text-warning me-2"></i>À propos de nous</h2>
-      <p><i class="bi bi-tools text-warning me-2"></i> Électricien de métier, nous mettons un point d’honneur à perpétuer les valeurs de l’artisanat : la qualité du travail, la rigueur et la confiance. Chaque intervention est réalisée avec soin, dans le respect des normes et des délais, pour garantir votre sécurité et votre confort.
+      <p className="text-justify"> <i class="bi bi-tools text-warning me-2"></i> Électricien de métier, nous mettons un point d’honneur à perpétuer les valeurs de l’artisanat : la qualité du travail, la rigueur et la confiance. Chaque intervention est réalisée avec soin, dans le respect des normes et des délais, pour garantir votre sécurité et votre confort.
       </p>
-      <p> <i class="bi bi-leaf text-warning me-2"></i>  Conscients des contraintes de la vie citadine et soucieux de notre impact environnemental, nous privilégions les déplacements à vélo dès que possible. Ce choix nous permet d’intervenir plus rapidement, tout en participant à l'amélioration de la qualité de vie urbaine.</p>
-      <p> <i class="bi bi-bicycle text-warning me-2"></i>  Allier savoir-faire, réactivité et mobilité douce, c’est notre manière d’exercer le métier d’électricien aujourd’hui — avec la même exigence qu’hier, et la responsabilité de demain.  </p> 
+      <p className="text-justify"> <i class="bi bi-bicycle text-warning me-2"></i>  Conscients des contraintes de la vie citadine et soucieux de notre impact environnemental, nous privilégions les déplacements à vélo dès que possible. Ce choix nous permet d’intervenir plus rapidement, tout en participant à l'amélioration de la qualité de vie urbaine.</p>
+      <p className="text-justify"> <i class="bi bi-leaf text-warning me-2"></i>  Allier savoir-faire, réactivité et mobilité douce, c’est notre manière d’exercer le métier d’électricien aujourd’hui — avec la même exigence qu’hier, et la responsabilité de demain.  </p> 
       <Galerie />
     </Container>
   );
