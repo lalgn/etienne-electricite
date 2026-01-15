649ca2d5ae9de8b99e1a610fc944079befe562db
etienne guerin <etien.guerin@gmail.com>
2026-01-15 11:04:47 +0100

    ajout d'une news sur tableau divisionnaire

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,6 +3,7 @@ import ServiceCard from "../components/ServiceCard";
 import { Container, Row, Col } from "react-bootstrap";
 import chantierimage from "../assets/renovation-bd-arr-cuisine.jpg"
 import chantierimage2 from "../assets/avant-apres-bd-arr-cuisine.jpg"
+import chantierimage3 from "../assets/avant-apres-tableau-divisionnaire.jpg"
 
 export const newsData = [
   {
@@ -14,7 +15,7 @@ export const newsData = [
     ),
     date: "13/11/2025",
     description: [
-      "Dans le cadre de la rénovation d'une arrière-cuisine, nous avons remplacé une vieille boite de dérivation en bois (!?) par une nouvelle boite de dérivation beaucoup plus classique :). Les vieux conducteurs ont été remplacés par des H07V-U dans des moulures."
+      "Dans le cadre de la rénovation d'une arrière-cuisine, nous avons remplacé une vieille boite de dérivation en bois (!?) par une nouvelle boite de dérivation beaucoup plus classique :). Les vieux conducteurs ont été remplacés par des H07V-U dans des moulures"
     ],
     image: chantierimage,
   },
@@ -30,6 +31,19 @@ export const newsData = [
       "La suite du précédent, après mise en peinture, cela méritait bien un avant-après..."
     ],
     image: chantierimage2,
+  },
+  {
+    title: (
+      <>
+        <i className="text-warning me-2"></i>
+        Chantier de renovation d'un tableau divisionnaire
+      </>
+    ),
+    date: "23/12/2025",
+    description: [
+      "Chantier de renovation d'un petit tableau divisionnaire qui était dangereux car il n'avait pas de borniers fixes"
+    ],
+    image: chantierimage3,
   },
 ];
 
